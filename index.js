c769a508a387b32903d841a282b964520a9402dc
潘仁杰 <3428675395@qq.com>
2023-01-04 16:12:33 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -36,13 +36,20 @@ window.addEventListener('resize',(res) => {
     console.log(browserheight)
     if(browserheight <= 770) {
         About_seven_days.style.height = '100%'
+        About_seven_days.style.width = '200px'
         About_seven_days.style.flexWrap = "wrap"
+        About_seven_days.style.margin = "0"
         About_seven_days.style.alignContent = "flex-start"
     }
     if(browserheight <= 850) {
+        About_seven_days.style.width = '200px'
         About_seven_days.style.height = '100%'
+        About_seven_days.style.margin = "0"
         About_seven_days.style.flexDirection = "column";
     } else {
+        About_seven_days.style.width = '100%'
+        About_seven_days.style.height = '150px'
+        About_seven_days.style.margin = "0 auto"
         About_seven_days.style.flexDirection = "row";
     }
 })

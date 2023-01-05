f6a76dcaa745c8072a4c310d0347bda6f30878da
潘仁杰 <3428675395@qq.com>
2023-01-05 09:52:44 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -9,31 +9,14 @@ window.onload = function () {
 const box = document.querySelector('#box')
 const Status_box = document.querySelector('#Status_box')
 const About_seven_days = document.querySelector('#About_seven_days')
-
+var today = {}
+var todaynature = {}
+var sevendaynature
+var sevenday = []
 var browserwidth = document.documentElement.clientWidth
 var browserheight = document.documentElement.clientHeight
-
-if(browserheight <= 770) {
-    About_seven_days.style.height = '100%'
-    About_seven_days.style.width = '200px'
-    About_seven_days.style.flexWrap = "wrap"
-    About_seven_days.style.margin = "0"
-    About_seven_days.style.alignContent = "flex-start"
-}
-if(browserheight <= 850) {
-    About_seven_days.style.width = '200px'
-    About_seven_days.style.height = '100%'
-    About_seven_days.style.margin = "0"
-    About_seven_days.style.flexDirection = "column";
-} else {
-    About_seven_days.style.width = '100%'
-    About_seven_days.style.height = '150px'
-    About_seven_days.style.margin = "0 auto"
-    About_seven_days.style.flexDirection = "row";
-}
-console.log(box)
+setElementCompatibility()
 box.addEventListener('mousemove',(e) => {
-    console.log(e)
     const [x,y] = [e.offsetX,e.offsetY]
     const [width,height] = [box.clientWidth,box.clientHeight]
     const sensitive = 20
@@ -47,24 +30,7 @@ box.addEventListener('mouseleave',() => {
 
 window.addEventListener('resize',(res) => {
     browserheight = document.documentElement.clientHeight
-    if(browserheight <= 770) {
-        About_seven_days.style.height = '100%'
-        About_seven_days.style.width = '200px'
-        About_seven_days.style.flexWrap = "wrap"
-        About_seven_days.style.margin = "0"
-        About_seven_days.style.alignContent = "flex-start"
-    }
-    if(browserheight <= 850) {
-        About_seven_days.style.width = '200px'
-        About_seven_days.style.height = '100%'
-        About_seven_days.style.margin = "0"
-        About_seven_days.style.flexDirection = "column";
-    } else {
-        About_seven_days.style.width = '100%'
-        About_seven_days.style.height = '150px'
-        About_seven_days.style.margin = "0 auto"
-        About_seven_days.style.flexDirection = "row";
-    }
+    setElementCompatibility()
 })
 const wearherObj = {
     qing: {
@@ -80,11 +46,26 @@ const wearherObj = {
         color:'linear-gradient(135deg,#00416a,#e4e5e6)'
     },
 }
+//元素自适应
+function setElementCompatibility() {
+    if(browserheight <= 770 || browserheight <= 850) {
+        About_seven_days.style.height = '100%'
+        About_seven_days.style.width = '200px'
+        About_seven_days.style.flexWrap = "wrap"
+        About_seven_days.style.margin = "0"
+        About_seven_days.style.alignContent = "flex-start"
+        About_seven_days.style.flexDirection = "column";
+    } else {
+        About_seven_days.style.width = '100%'
+        About_seven_days.style.height = '150px'
+        About_seven_days.style.margin = "0 auto"
+        About_seven_days.style.flexDirection = "row";
+    }
+}
 //获取天气
 function getWeather() {
     fetch('https://www.yiketianqi.com/free/day?appid=97285255&appsecret=yJaVDb7F&unescape=1')
         .then(res => {
-            console.log(res);
             let { status, statusText } = res
             if (status == 200) {
                 return res.json()
@@ -104,19 +85,17 @@ function getWeather() {
 <!--                    <p>当前星期：${data.week}</p>-->
                 `
             console.log(data)
-            console.log(Status_box)
+            today = data
             //xue、lei、shachen、bingbao、yu、yin
-
             Status_box.style.background = wearherObj[data.wea_img].color
-            console.log(wearherObj[data.wea_img].img)
-            UpWeather(wearherObj[data.wea_img].img,'bm')
+            var de =  UpWeather(wearherObj[data.wea_img].img,'bm',todaynature)
+            console.log(de)
         });
 }
 //获取近七天天气
 function getDailyHotSearches() {
     fetch('https://v0.yiketianqi.com/free/week?appid=97285255&appsecret=yJaVDb7F&unescape=1')
         .then(res => {
-            console.log(res);
             let { status, statusText } = res
             if (status == 200) {
                 return res.json()
@@ -124,25 +103,53 @@ function getDailyHotSearches() {
         })
         .then(data => {
             console.log(data)
+            sevenday = data.data
             var arr = []
             data.data.forEach((item,index) => {
                 arr.push(`
                     <div class="days">
                         <div id="wath${(index+1)}">
                             <div class="daydata">
-                                <p>${item.tem_day}℃/${item.tem_night}℃ <br> ${item.win}/${item.win_speed}</p>
+                                <p>${item.wea} <br> ${item.tem_day}℃/${item.tem_night}℃ <br> <span>${item.win}/${item.win_speed}</span> </p>
                             </div>
                         </div>
                     </div>`)
             })
             About_seven_days.innerHTML = arr.join('')
             var dayChildren = Array.from(About_seven_days.children)
-            console.log(dayChildren)
             dayChildren.forEach((item ,index)=> {
-                console.log(item)
                 item.addEventListener('click', (e) => {
+                    var operate
                     console.log(e)
                     console.log(index)
+                    console.log(sevenday[index])
+                    Status_box.style.background = wearherObj[sevenday[index].wea_img].color
+                    if(index == 0) {
+                        weather.innerHTML = `
+                            <p>当前城市：${today.city}</p>
+                            <p>当前城市天气：${today.wea}</p>
+                            <p>当前城市湿度：${today.humidity}</p>
+                            <p>当前城市气压：${today.pressure}㍱</p>
+                            <p>当前城市空气质量：${today.air}</p>
+                            <p>当前城市今天气温：${today.tem}℃</p>
+                            <p>城市今天最高气温/最低气温：${today.tem_day}℃/${today.tem_night}℃</p>
+                            <p>当前风向/风速：${today.win}/${today.win_speed}</p>
+                        `
+                        operate = UpWeather(wearherObj[sevenday[index].wea_img].img,'bm',todaynature)
+                        operate.destroy(todaynature)
+                        UpWeather(wearherObj[sevenday[index].wea_img].img,'bm',todaynature)
+                        return
+                    }
+                    weather.innerHTML = `
+                        <p>当日日期：${sevenday[index].date}</p>
+                        <p>当前城市：${data.city}</p>
+                        <p>当前城市当日天气：${sevenday[index].wea}</p>
+                        <p>城市当日最高气温/最低气温：${sevenday[index].tem_day}℃/${sevenday[index].tem_night}℃</p>
+                        <p>当日风向/风速：${sevenday[index].win}/${sevenday[index].win_speed}</p>
+                    `
+                    operate = UpWeather(wearherObj[sevenday[index].wea_img].img,'bm',todaynature)
+                    operate.destroy(todaynature)
+                    UpWeather(wearherObj[sevenday[index].wea_img].img,'bm',todaynature)
                 })
             })
             data.data.forEach((item,index) => {
@@ -153,12 +160,14 @@ function getDailyHotSearches() {
         });
 }
 //更新当前天气情况
-function UpWeather(path,element) {
+function UpWeather(path,element,nature) {
     bodymovin.loadAnimation({
         container: document.getElementById(element),
         renderer: 'svg',
         loop: true,
         autoplay: true,
-        path: path
+        path: path,
+        name:nature
     })
+    return bodymovin
 }
